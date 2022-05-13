897882efebf90f029d186420f89a0ad5f29f37ce
asiimwepaul <asiimwepaul23@gmail.com>
2022-05-13 13:05:00 +0300

    create a fullstack project

---
+++ client/src/components/showTodoList.jsx
@@ -0,0 +1,12 @@
+import { ShowTodoList } from "./components/showTodoList";
+import "./App.scss";
+
+function App() {
+    return(
+        <div>
+            <ShowTodoList />
+        </div>
+    )
+}
+
+export default App;
