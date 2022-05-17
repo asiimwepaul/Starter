c6c63a86de0e49a9299e32eaffb6b43231864323
asiimwepaul <asiimwepaul23@gmail.com>
2022-05-17 08:26:51 +0300

    modify showTodoList file

--- client/src/components/showTodoList.jsx
+++ client/src/components/showTodoList.jsx
@@ -1,12 +1,76 @@
-import { ShowTodoList } from "./components/showTodoList";
-import "./App.scss";
+import { useState } from "react";
+import { link } from "react-router-dom";
 
-function App() {
-    return(
-        <div>
-            <ShowTodoList />
-        </div>
-    )
+import axios from "axios";
+
+export function CreateTodo() {
+    const [data, setData] = useState({ title: "", description: "" });
+
+    function handleChange(e) {
+       setData((data) =>({ ...data, [e.target.name]: e.target.value }));
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const todo = {
+            title: data.title,
+            description: data.description,
+        };
+
+        console.log({ todo });
+        axios
+            .post("http://localhost:8000/api/todo", data)
+            .then((res) => {
+                setData({ title: "", description: "" });
+                console.log(res.data.message);
+            })
+            .catch((err) => {
+                console.log("Error couldn't create TODO");
+                console.log(err.message);
+            });
+        
+    }
+
+    return (
+        <section className="container">
+            <link to="/" className="button-back">
+                <button type="button" className="button">
+                    back
+                </button>
+            </link>
+            <section className="contents">
+                <form
+                    onSubmit={handleSubmit}
+                    className="form-container"
+                    onValidate
+                >
+                    <label className="label" htmlFor="title">
+                        Title
+                    </label>
+                    <input
+                        className="input"
+                        type="text" 
+                        name="title"
+                        value={data.title}
+                        onChange={handleChange}
+                        />
+                    <label className="label" htmlFor="description">
+                        Description
+                    </label>
+                    <input 
+                        className="input" 
+                        type="text"
+                        name="description"
+                        value={data.description}
+                        onChange={hangleChange}
+
+                        />
+                    <button type="submit" className="button">
+                        create Todo
+                    </button>
+                </form>
+            </section>
+        </section>
+    );
 }
-
-export default App;
